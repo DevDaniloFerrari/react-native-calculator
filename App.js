b630a71e71c12d6cf139e52f1897ccd99a8986dd
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2021-01-05 14:13:39 -0300

    implementando display

--- App.js
+++ App.js
@@ -5,11 +5,17 @@ import {
   View
 } from 'react-native';
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 export default class App extends Component {
+  state = {
+    displayValue: '0'
+  }
+
   render() {
     return (<>
       <SafeAreaView style={styles.container}>
+        <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
           <Button label='AC'></Button>
           <Button label='/'></Button>
